ffe9fd4fb11e09fb55b06fab1edfda8aaf381bb8
Yamauchi Koyo <hi.koko.95.s@gmail.com>
2026-05-19 14:10:41 +0900

    prepare render deploy

--- app.py
+++ app.py
@@ -296,5 +296,11 @@ def delete_file(filename):
 # 起動
 # ------------------------
 if __name__ == "__main__":
-    init_db()
-    app.run(debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
